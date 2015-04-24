879709b6f560d1dc4e2940e4eab3ae540348d4ed
hesers <steve.heser@gmail.com>
2015-04-24 11:42:29 -0500

    Update boopsie.bat

--- boopsie.bat
+++ boopsie.bat
@@ -1,6 +1,6 @@
 REM delete whatever old file exists
 DEL "C:\scripts\boopsie\*.*" /Q
-REM Run the sqlworkbench scripts to export cataloging data to MySQL.
+REM Run the sqlworkbench scripts to create the new Boopsie file.
 java -jar C:\SQLWorkbench\sqlworkbench.jar script='c:/scripts/boopsie.sql' -profile=CountyCat -showProgress=true
 REM Compress file with 7zip
 CD C:\7za920
