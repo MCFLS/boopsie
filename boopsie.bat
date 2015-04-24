5ad1622a220c0ccfed1499ce0824f7b1feb5e8b6
hesers <steve.heser@gmail.com>
2015-04-24 10:57:01 -0500

    Create boopsie.bat
    Boopsie BAT file that runs the query and sends data to Boopsie

---
+++ boopsie.bat
@@ -0,0 +1,9 @@
+REM delete whatever old file exists
+DEL "C:\scripts\boopsie\*.*" /Q
+REM Run the sqlworkbench scripts to export cataloging data to MySQL.
+java -jar C:\SQLWorkbench\sqlworkbench.jar script='c:/scripts/boopsie.sql' -profile=CountyCat -showProgress=true
+REM Compress file with 7zip
+CD C:\7za920
+7za a "C:\scripts\boopsie\catalog_extract_full.7z" "C:\scripts\boopsie\catalog_extract_full.txt"
+REM Run winscp to ftp the files over to Boopsie
+winscp.com /command "option batch abort" "option confirm off" "open boopsie" "put C:\scripts\boopsie\catalog_extract_full.7z" "exit"
